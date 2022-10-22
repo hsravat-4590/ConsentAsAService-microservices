5b63f4f74c2d72d725c388925ac152b0f9aace3d
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2022-10-22 12:25:53 +0100

    Inital SAP TransactionService

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,4 +19,11 @@ include(":Libraries:DA-Lib:MySQL")
 project(":Libraries:DA-Lib:MySQL").name = "DA-Lib-MySQL"
 include(":Libraries:DA-Lib:API")
 project(":Libraries:DA-Lib:API").name = "DA-Lib-API"
-
+include("Services:SAP-Transaction-Service")
+findProject(":Services:SAP-Transaction-Service")?.name = "SAP-Transaction-Service"
+include("Services:SAP-Transaction-Service:DA")
+findProject(":Services:SAP-Transaction-Service:DA")?.name = "SAP-Transaction-Service-DA"
+include("Services:SAP-Transaction-Service:API")
+findProject(":Services:SAP-Transaction-Service:API")?.name = "SAP-Transaction-Service-API"
+include("Services:SAP-Transaction-Service:IMPL")
+findProject(":Services:SAP-Transaction-Service:IMPL")?.name = "SAP-Transaction-Service-IMPL"
