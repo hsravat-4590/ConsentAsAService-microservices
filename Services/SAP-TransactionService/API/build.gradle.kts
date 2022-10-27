8adbea2b6e3fccf7037c1638cf888df00fcba1ec
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2022-10-27 14:26:38 +0100

    Fix build issue

--- Services/SAP-TransactionService/API/build.gradle.kts
+++ Services/SAP-TransactionService/API/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    id("io.spring.dependency-management") apply true
     kotlin("jvm")
     kotlin("plugin.spring")
 }
@@ -13,10 +14,8 @@ configurations {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:2.7.5")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
-    compileOnly("org.projectlombok:lombok")
-
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    annotationProcessor("org.projectlombok:lombok")
+    compileOnly("org.projectlombok:lombok:1.18.24")
+    annotationProcessor("org.projectlombok:lombok:1.18.24")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
