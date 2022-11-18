3dc6d55f140a011d9c05aa250b9cead4ec3b6985
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2022-11-18 15:54:29 +0000

    Boot config processor version

--- buildSrc/src/main/kotlin/com/ravathanzalah/consentservice/gradle/spring/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/ravathanzalah/consentservice/gradle/spring/Dependencies.kt
@@ -4,7 +4,7 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
 
 const val SPRING_FRAMEWORK_VERSION = "5.3.23"
-const val SPRING_CONFIG_PROCESSOR_VERSION = "2.7.4"
+const val SPRING_CONFIG_PROCESSOR_VERSION = "2.7.5"
 
 val coreSpringImplementations = listOf(
     "org.springframework:spring-core:$SPRING_FRAMEWORK_VERSION",
