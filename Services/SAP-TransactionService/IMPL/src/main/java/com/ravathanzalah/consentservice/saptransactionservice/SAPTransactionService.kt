02e400f6dc1bb640732f45a4f9bc2a3f3fcfcf28
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2022-11-18 15:57:25 +0000

    Add Readme

--- Services/SAP-TransactionService/IMPL/src/main/java/com/ravathanzalah/consentservice/saptransactionservice/SAPTransactionService.kt
+++ Services/SAP-TransactionService/IMPL/src/main/java/com/ravathanzalah/consentservice/saptransactionservice/SAPTransactionService.kt
@@ -2,8 +2,10 @@ package com.ravathanzalah.consentservice.saptransactionservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class SAPTransactionService
 
 fun main(args: Array<String>) {
