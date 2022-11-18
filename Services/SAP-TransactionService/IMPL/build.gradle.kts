d93a208a97151a2346bc8e0825b07a981ac20f5a
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2022-11-18 15:58:06 +0000

    SAP-TransactionService, add support for a Eureka Client

--- Services/SAP-TransactionService/IMPL/build.gradle.kts
+++ Services/SAP-TransactionService/IMPL/build.gradle.kts
@@ -13,25 +13,35 @@ configurations {
     }
 }
 
-springBoot {
-    mainClass.set("com.ravathanzalah.consentservice.saptransactionservice.SAPTransactionService")
+
+tasks.getByName<Jar>("jar") {
+    enabled = false
 }
 
-
+tasks.bootJar {
+    enabled = true
+}
 
 dependencies {
     implementation(project(":Services:SAP-TransactionService:SAP-TransactionService-DA"))
     implementation(project(":Services:SAP-TransactionService:SAP-TransactionService-API"))
-    implementation("org.springframework.boot:spring-boot-starter-web:2.7.5")
-    implementation("org.springframework.boot:spring-boot-starter-actuator:2.7.5")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
-    implementation("org.springframework.statemachine:spring-statemachine-core:3.2.0")
-    implementation("org.springframework.webflow:spring-webflow:2.5.1.RELEASE")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+extra["springCloudVersion"] = "2021.0.5"
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
 
 tasks.getByName<Test>("test") {
